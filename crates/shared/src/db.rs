b3f206136e0fde5af09c33eac1060c5b6779ab82
Tiago Oliveira <tiagoco2008@gmail.com>
2026-05-13 11:18:51 +0100

    feat(src): add db

---
+++ crates/shared/src/db.rs
@@ -0,0 +1,7 @@
+use sea_orm::{Database, DatabaseConnection};
+
+pub async fn establish_connection(database_url: &str) -> Result<DatabaseConnection, sea_orm::DbErr> {
+    let db = Database::connect(database_url).await?;
+    tracing::info!("Database connection established");
+    Ok(db)
+}
